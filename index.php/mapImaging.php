6df433ace7d8600bfac1b776ad2ee1524d1f0a5e
Kevin Hsieh <hsieh63@eden.rutgers.edu>
2013-03-29 03:41:21 -0400

    Edited map image file
    testing image work

--- index.php/mapImaging.php
+++ index.php/mapImaging.php
@@ -1,13 +1,15 @@
 ﻿<?php
-include 'sqlQueries.php';
+//include 'sqlQueries.php';
 $zipcode = $_POST["zipcode"];
 //mapTrafficPoints();
 //key: AIzaSyAdM76kzhZ0uwCyHxZLogbt5Sc9PrF1RpM
 //linkg: http://maps.googleapis.com/maps/api/staticmap?center=New+York,NY&zoom=13&size=600x300&key=API_console_key
-$googleImgSrc = "http://maps.googleapis.com/maps/api/staticmap?center=$zipcode&zoom=13&size=640x640&scale=2&key=AIzaSyAdM76kzhZ0uwCyHxZLogbt5Sc9PrF1RpM";
+$googleImgSrc = "http://maps.googleapis.com/maps/api/staticmap?center=" . $zipcode . "&zoom=13&size=640x640&scale=2&key=AIzaSyAdM76kzhZ0uwCyHxZLogbt5Sc9PrF1RpM";
 ?>
 <?php include 'header.php'; ?>
     <div align="center">
+    Testing
+    <br>
         <img src="<?php echo $googleImgSrc; ?>" alt="Map Image of <?php echo $zipcode; ?>">
     </div>
 <?php include 'footer.php'; ?>
