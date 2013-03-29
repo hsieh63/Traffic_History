103fc78832d4872896ffe32a4ec0eadfa811ea66
Kevin Hsieh <hsieh63@eden.rutgers.edu>
2013-03-29 03:35:12 -0400

    Adding map image file

---
+++ index.php/sqlQueries.php
@@ -0,0 +1,19 @@
+﻿<?php
+
+function mapTrafficPoints() {
+    //Create connection
+    //mysqli_connect(host,username,password,dbname);
+    $con = mysqli_connect("fdb4.biz.nf","1270538_traffic","Software2013","1270538_traffic");
+
+    if (mysqli_connect_errno($con))
+    {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    
+    //$res = $con->query("");
+    //parse return
+    //return x;
+    
+    mysqli_close($con);
+}
+?>
