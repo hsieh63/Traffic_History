a3d8393bad8be0995b6bce3a4499c05b10ac59ae
Kevin Hsieh <hsieh63@eden.rutgers.edu>
2013-03-30 23:13:21 -0400

    Bug in map imaging
    double response which would return 2 maps

--- index.php/mapImaging.php
+++ index.php/mapImaging.php
@@ -8,15 +8,9 @@ if(isset($_POST['zipcode'])) {
     //key: AIzaSyAdM76kzhZ0uwCyHxZLogbt5Sc9PrF1RpM
     //linkg: http://maps.googleapis.com/maps/api/staticmap?center=New+York,NY&zoom=13&size=600x300&key=API_console_key
     $googleImgSrc = "http://maps.googleapis.com/maps/api/staticmap?center=" . $zipcode . "&zoom=10&size=640x640&scale=2&key=AIzaSyAdM76kzhZ0uwCyHxZLogbt5Sc9PrF1RpM&sensor=false";
-    $htmlImg = "<img id='mapImg' src= '" . $googleImgSrc . "' alt='Map Image of " . $zipcode . "'><label id='zoomLvl' value='10' style='display:none;'></label>";
-    echo $htmlImg;
+    //can either echo or just construct it in html
+    //$htmlImg = "<img id='mapImg' src= '" . $googleImgSrc . "' alt='Map Image of " . $zipcode . "'><label id='zoomLvl' value='10' style='display:none;'></label>";
+    //echo $htmlImg;
 }
 ?>
-
-//<?php include 'header.php'; ?>
-//    <div align="center">
-//    Testing
-//    <br>
-//        <img src="<?php echo $googleImgSrc; ?>" alt="Map Image of <?php echo $zipcode; ?>">
-//    </div>
-//<?php include 'footer.php'; ?>
+<img id='mapImg' src= '<?php echo $googleImgSrc ?>' alt='Map Image of <?php echo $zipcode ?>'><label id='zoomLvl' value='10' style='display:none;'></label>";
