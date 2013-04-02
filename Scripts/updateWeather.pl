f3d6803d87b56f9742a847139abb08e6925efa78
Matt Araneta <matt.b.araneta@gmail.com>
2013-04-01 23:08:35 -0700

    Syntax changes, removed unnecessary stuff

--- Scripts/updateWeather.pl
+++ Scripts/updateWeather.pl
@@ -5,17 +5,15 @@ use DBI;
 use DBD::ODBC;
 
 #Config variables
-my $dsn = "DBI:ODBC:Driver=(SQL Server)"; #might have to change
-my $host = '10.0.0.1,1433'; #change
-#my $port = ' '; 
-my $database = 'databasename';
-my $user = 'Kevin';
+my $dsn = "DBI:ODBC:Driver={SQL Server}"; #might have to change
+my $host = '10.0.0.1,1433';		#change to server name
+my $database = 'databasename';  #change to database name
+my $user = 'Kevin';				#database user name
 my $auth = 'kevinblah';
 
-#Use DBD::mysql module to connect to SQL database 
+#Use DBD::OBDC module to connect to SQL database 
 
 my $dbh = DBI->connect('$dsn;Server=$host;Database=$database',
-					   #'host=$host;port=$port', i don't know if you need this
 						$user,
 						$auth,  #might have different parameters
 					  ) || die "Database connection not made: $DBI::errstr";
