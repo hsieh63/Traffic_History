34d8b7ed123a8b90478654173373f2d643d9977c
Kevin Hsieh <hsieh63@eden.rutgers.edu>
2013-04-02 01:29:53 -0400

    Fixed database insertion to work on personal computer
    done by matt, compiled on kevin's computer

--- Scripts/updateWeather.pl
+++ Scripts/updateWeather.pl
@@ -5,22 +5,22 @@ use DBI;
 use DBD::ODBC;
 
 #Config variables
-my $dsn = "DBI:ODBC:Driver={SQL Server}"; #might have to change
-my $host = '10.0.0.1,1433';		#change to server name
-my $database = 'databasename';  #change to database name
-my $user = 'Kevin';				#database user name
-my $auth = 'kevinblah';
+my $dsn = "localDSN"; #might have to change
+my $host = 'Q6600\Q6600MSSQL';		#change to server name
+my $database = 'trafficHistory';  #change to database name
+my $user = 'Q6600\kev';				#database user name
+my $auth = '';
 
 #Use DBD::OBDC module to connect to SQL database 
 
-my $dbh = DBI->connect('$dsn;Server=$host;Database=$database',
+my $dbh = DBI->connect('DBI:ODBC:localDSN',
 						$user,
 						$auth,  #might have different parameters
 					  ) || die "Database connection not made: $DBI::errstr";
 					  
 #Prepare the SQL insertion - just test that it works
-my $SQLinsert = "INSERT INTO table_name (zipcode, weather)" .
-				 "VALUES (08873, Sunny)";
+my $SQLinsert = "INSERT INTO Weather (zipcode, weather)" .
+				 "VALUES (08873, 'Sunny')";
 my $query_handle = $dbh->prepare($SQLinsert);
 				 
 #Will actually be parsing a text file and inserting many rows
