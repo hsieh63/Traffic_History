1b46ae4a8b03af63e5758385fd8ce55a9dd05b42
Kevin Hsieh <hsieh63@eden.rutgers.edu>
2013-04-05 17:18:50 -0400

    Put menu in its own php file

--- index.php/header.php
+++ index.php/header.php
@@ -13,27 +13,4 @@
     <script src="http://code.jquery.com/jquery-migrate-1.0.0.js"></script>
 	<title>Traffic Monitoring History</title>
 </head>
-<body>
-<div id="menu" align="center">
-    <ul class="menu">
-        <li><a href="index.php" class="parent"><span>Home</span></a></li>
-        <li><a href="/map.php" class="parent"><span>Map</span></a>
-            <div><ul>
-                <li><a href="#" class="parent"><span>Sub Item 1</span></a>
-                    <div><ul>
-                        <li><a href="#"><span>Sub Item 1.1</span></a></li>
-                        <li><a href="#"><span>Sub Item 1.2</span></a></li>
-                    </ul></div>
-                </li>
-                <li><a href="#" class="parent"><span>Sub Item 2</span></a>
-                    <div><ul>
-                        <li><a href="#"><span>Sub Item 2.1</span></a></li>
-                        <li><a href="#"><span>Sub Item 2.2</span></a></li>
-                    </ul></div>
-                </li>
-            </ul></div>
-        </li>
-        <li><a href="/progress.php" class="parent"><span>Project Progress</span></a></li>
-        <li class="last"><a href="/groupMember.php"><span>Group Members</span></a></li>
-    </ul>
-</div>
+<body><?php include 'menu.php' ?>
---
+++ index.php/menu.php
@@ -0,0 +1,23 @@
+﻿<div id="menu" align="center">
+    <ul class="menu">
+        <li><a href="index.php" class="parent"><span>Home</span></a></li>
+        <li><a href="/map.php" class="parent"><span>Map</span></a>
+            <div><ul>
+                <li><a href="#" class="parent"><span>Sub Item 1</span></a>
+                    <div><ul>
+                        <li><a href="#"><span>Sub Item 1.1</span></a></li>
+                        <li><a href="#"><span>Sub Item 1.2</span></a></li>
+                    </ul></div>
+                </li>
+                <li><a href="#" class="parent"><span>Sub Item 2</span></a>
+                    <div><ul>
+                        <li><a href="#"><span>Sub Item 2.1</span></a></li>
+                        <li><a href="#"><span>Sub Item 2.2</span></a></li>
+                    </ul></div>
+                </li>
+            </ul></div>
+        </li>
+        <li><a href="/progress.php" class="parent"><span>Project Progress</span></a></li>
+        <li class="last"><a href="/groupMember.php"><span>Group Members</span></a></li>
+    </ul>
+</div>
