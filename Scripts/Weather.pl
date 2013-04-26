2c2194018b772bc9c11c0576e0db662d1522d7a7
John Reed <johnreed8@gmail.com>
2013-04-25 22:23:19 -0400

    Revert "Remove bad zip code from query"
    This reverts commit a2e60029420bfada7448ceda137854a164b4c9fd.

--- Scripts/Weather.pl
+++ Scripts/Weather.pl
@@ -12,9 +12,8 @@ use HTTP::Request;
 use JSON;
 use DBI;
 
+	
 #Config variables - for now, can only compile on Kevin's local computer 
-my $dsn = "localDSN"; 			  #Data Source Name - 'localDSN' is specific to Kevin's computer
-my $host = 'Q6600\Q6600MSSQL';		#change to server name
 my $database = 'DBI:mysql:traffich_main';  #change to database name
 my $user = 'traffich_admin';				#database user name
 my $auth = 'Admin2013';				#user password
@@ -27,175 +26,115 @@ my $dbh = DBI->connect($database,
 			) || die "Database connection not made: $DBI::errstr";
 					  
 #Prepare the SQL insertion query- COLUMN NAMES LISTED MUST MATCH DATABASE COLUMN NAMES
-my $SQLinsert = "INSERT INTO Weather (Zipcode, Weather, Date_Time)" .
-				 "VALUES (?, ?, ?)";
-my $query_handle; 	#Initialize query handle - define later when it is necessary
+my $SQLinsert = "INSERT INTO Weather (Zipcode, Weather) VALUES (?, ?)";
 
 #query to get 1 zip code per county
-#my $SQLreceive = "SELECT max(Zipcode) FROM `Zip_Codes` WHERE 1 GROUP BY County";
-my $SQLreceive = "SELECT max(Zipcode) FROM `Zip_Codes` WHERE Zipcode NOT IN 
-				(SELECT Zipcode FROM `Zip_Codes` WHERE Zipcode=19640)
-				GROUP BY County";
-
+my $SQLreceive = "SELECT max(Zipcode) FROM `Zip_Codes` WHERE 1 GROUP BY County";
 #prepare statement
 my $sth = $dbh->prepare($SQLreceive) or die "Couldn't prepare statement: $DBI->errstr; stopped";
-#execute the query
-$sth->execute() or die "Couldn't prepare statement: $DBI->errstr; stopped";
+my $query_handle = $dbh->prepare($SQLinsert) or die "Couldn't prepare statement: " . $dbh->errstr;
 
-# script will gather data at specific time intervals
-=begin
-while(true){
-	loopThroughZips();
-	sleep(60); #sleep for 60 seconds
-}	
-=cut
-
-#Loops through zip codes and accesses the getWeather function
-sub loopThroughZips(){
-	#while there is still a zipcode left in the SQL query
-	while ( my ($Zipcode) = $sth->fetchrow_arrayref() ) {
-		#execute function with parameter from database
-		getWeather($Zipcode);
-	}
-}
 
 #needs a zipcode parameter passed in to function
-sub getWeather(){
+sub getWeather{
 	# opens zip code input file and parses for locations
 	# zip codes in input file missing the first 0s, but website accepts anyway
 	
 	#take Zip Code as the first and only parameter
-	my($zip) = $_[0];
-	
-	#comment out old code getting zipcodes from txt file
-	=begin comment
-	
-	if (open(FILE, "zipCodeTestFile.txt")) {
+	my $zipcode = $_[0];
 		
-		open OUTPUT, ">getWeatherOutput.txt";
-		
-		while (<FILE>) {
-			
-			  $zip="$_";
-			  print "ZIP CODE: 0$_\n";
-			  print OUTPUT "ZIP CODE: 0$_\n";
-			  getInfo();
-			  print "\n\n";
+	# converts the data in the JSON object into a hash table
+	my $ua = LWP::UserAgent->new; #creates user agent to get http
+	my $url = "http://api.wunderground.com/api/e5d1818deab8c1a3/conditions/q/$zipcode.json"; #sets up url, this needs to be dynamic
 
-		}
-		print "\n";
-		close (FILE);
-		close OUTPUT;
-	} else {
-		print "Cannot open file!\n";
-	#	exit 1;
-	}
+	my $req = HTTP::Request->new(POST => $url); #requesting url
+	my $jsonResponse = JSON->new; #initilize json object
+	my $response = $ua->request($req); #get response to url?
+	$jsonResponse = $response->content; #set content which is a json into json object
+	my $hashRef = decode_json $jsonResponse; #decode json using module and gets a hash reference number
+	my %hash = %$hashRef; #deference hash number to a hash
 	
-	=end comment 
-	=cut
-	
-	sub getInfo() {
 
-		# converts the data in the JSON object into a hash table
-		my $ua = LWP::UserAgent->new; #creates user agent to get http
-		my $url = "http://api.wunderground.com/api/e5d1818deab8c1a3/conditions/q/${zip}.json"; #sets up url, this needs to be dynamic
-		#my $url = 'http://api.wunderground.com/api/e5d1818deab8c1a3/conditions/q/CA/San_Francisco.json';
 
-		my $req = HTTP::Request->new(POST => $url); #requesting url
-		my $jsonResponse = JSON->new; #initilize json object
-		my $response = $ua->request($req); #get response to url?
-		$jsonResponse = $response->content; #set content which is a json into json object
-		my $hashRef = decode_json $jsonResponse; #decode json using module and gets a hash reference number
-		my %hash = %$hashRef; #deference hash number to a hash
+	if ($response->is_success) {
+		my %hashNest = %{$hash{"current_observation"}};			
 		
-
-
-		if ($response->is_success) {
-			my %hashNest = %{$hash{"current_observation"}};			
-			
-			#DATABASE IMPLEMENTATION - the data we need is to be stored in our database
-			#define variables which will be stored in the database
-			my $zipcode = $zip;							#Zip Code
-			my $description = $hashNest{"icon"};		#Description of Incident
-			my $time = $hashNest{"observation_time"};	#Time incident occured
-			my $converted_time = convertTime($time);	#Converted integer from 0-7 (see subscript convertTime)
-			
-			#if the query handle is not yet defined, define it here (prevents the overhead of defining it in every loop)
-			if(! defined $query_handle) {
-				$query_handle = $dbh->prepare($SQLinsert)
-					or die "Couldn't prepare statement: " . $dbh->errstr;
-			}
-			
-			#execute the query handle
-			#executes the SQL query defined by the handle and stores the given variables in the database using the DBI
-			$query_handle->execute($zipcode, $description, $converted_time);
-			
-		}
-		else {
-			print $response->status_line. " Fail\n";
-		}
+		#DATABASE IMPLEMENTATION - the data we need is to be stored in our database
+		#define variables which will be stored in the database
+		my $description = $hashNest{"icon"};		#Description of Incident
+		#my $time = $hashNest{"observation_time"};	#Time incident occured
+		#my $converted_time = convertTime($time);	#Converted integer from 0-7 (see subscript convertTime)
+		
+		
+		#execute the query handle
+		#executes the SQL query defined by the handle and stores the given variables in the database using the DBI
+		$query_handle->execute($zipcode, $description);
 		
 	}
+	else {
+		print $response->status_line. " Fail\n";
+	}
+		
 }
 
 
-	#Converts the string obtained from the API into an integer from 0-7
-	#The string is in the format "Last updated on 12:35 PM"
-	#12-2:59AM = 1, 3-5:59AM = 2, ... , 9-11:59PM = 7
-	
-	sub convertTime{
-	my $obs_time = $_[0];
-		if($obs_time =~ /((\d)+):\d\d\s((AM|PM))/) {	#checks if the format is in the string obtained
-			my $hour = $1;
-			my $period = $3;
+#Converts the string obtained from the API into an integer from 0-7
+#The string is in the format "Last updated on 12:35 PM"
+#12-2:59AM = 1, 3-5:59AM = 2, ... , 9-11:59PM = 7
 
-			#Returns an integer from 0-3 if period is AM
-			if($period eq "AM") {
-				#Chooses an integer depending on the hour
-				if ($hour == 12 or $hour == 1 or $hour == 2){
-					return 0;
-				}
-				if ($hour == 3 or $hour == 4 or $hour == 5){
-					return 1;
-				}
-				if ($hour == 6 or $hour == 7 or $hour == 8){
-					return 2;
-				}
-				if ($hour == 9 or $hour == 10 or $hour == 11){
-					return 3;
-				}
+sub convertTime{
+my $obs_time = $_[0];
+	if($obs_time =~ /((\d)+):\d\d\s((AM|PM))/) {	#checks if the format is in the string obtained
+		my $hour = $1;
+		my $period = $3;
+
+		#Returns an integer from 0-3 if period is AM
+		if($period eq "AM") {
+			#Chooses an integer depending on the hour
+			if ($hour == 12 or $hour == 1 or $hour == 2){
+				return 0;
 			}
-			
-			#Returns an integer from 4-7 if the period is PM
-			if($period eq "PM") {
-				#Chooses an integer depending on the hour
-				if ($hour == 12 or $hour == 1 or $hour == 2){
-					return 4;
-				}
-				if ($hour == 3 or $hour == 4 or $hour == 5){
-					return 5;
-				}
-				if ($hour == 6 or $hour == 7 or $hour == 8){
-					return 6;
-				}
-				if ($hour == 9 or $hour == 10 or $hour == 11){
-					return 7;
-				}			
+			if ($hour == 3 or $hour == 4 or $hour == 5){
+				return 1;
+			}
+			if ($hour == 6 or $hour == 7 or $hour == 8){
+				return 2;
+			}
+			if ($hour == 9 or $hour == 10 or $hour == 11){
+				return 3;
 			}
 		}
-		else{
-			print "Error";
+		
+		#Returns an integer from 4-7 if the period is PM
+		if($period eq "PM") {
+			#Chooses an integer depending on the hour
+			if ($hour == 12 or $hour == 1 or $hour == 2){
+				return 4;
+			}
+			if ($hour == 3 or $hour == 4 or $hour == 5){
+				return 5;
+			}
+			if ($hour == 6 or $hour == 7 or $hour == 8){
+				return 6;
+			}
+			if ($hour == 9 or $hour == 10 or $hour == 11){
+				return 7;
+			}			
 		}
 	}
+	else{
+		print "Error";
+	}
+}
+#execute the query
+$sth->execute() or die "Couldn't prepare statement: $DBI->errstr; stopped";
 
-		
-	
-
-#main function
-loopThroughZips();
-
+#while there is still a zipcode left in the SQL query
+while ( my $Zipcode = $sth->fetchrow_arrayref() ) {
+	#execute function with parameter from database
+	getWeather(${$Zipcode}[0]);
+	sleep(7);
+}
 
 #Finished querying, close the connection
-$sth->finish();
-$query_handle->finish();
 $dbh->disconnect();
