8232810a7fe14f15accdc87b0fe4f9e544372b56
Kevin Hsieh <hsieh63@eden.rutgers.edu>
2013-05-01 22:47:11 -0400

    Fully operational
    Added update, also throttle it to 1 per 10 seconds, dont know google's
    rate limit

--- Scripts/googleReverseGeo.pl
+++ Scripts/googleReverseGeo.pl
@@ -30,7 +30,7 @@ my $dbh = DBI->connect( $dataSource, $dbUser, $dbPass )
 my $sth =
   $dbh->prepare("SELECT * FROM `Traffic_Mapquest` WHERE Address LIKE ''") 
   or die "Couldn't prepare statemenent: " . $dbh->errstr;
-my $sth2 = $dbh->prepare("UPDATE `Traffic_Mapquest` SET Address = ? WHERE Index = ?")
+my $sth2 = $dbh->prepare("UPDATE `Traffic_Mapquest` SET Address = ? WHERE `Index` = ?")
   or die "Couldn't prepare statemenent: " . $dbh->errstr;
 
 $sth->execute() or die "Couldn't execute statement: " . $sth->errstr;
@@ -40,31 +40,29 @@ while ( my $rowHash = $sth->fetchrow_hashref() ) {
 	my $index = $rowHash->{"Index"};
 	
 	my $ua = LWP::UserAgent->new;    #creates user agent to get http
-
-	my $url =
-"http://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$long&sensor=false";
-
+	my $url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$long&sensor=false";
 	#print FILE "url : $url\n";
+	sleep(10);
 
-	#sets up url, this needs to be dynamic
-
-	my $req = HTTP::Request->new( POST => $url );    #requesting url
+	my $req = HTTP::Request->new( GET => $url );    #requesting url
 	my $jsonResponse = JSON->new;                    #initilize json object
 	my $response     = $ua->request($req);           #get response to url?
 	$jsonResponse = $response->content;
-	my $hashRef =
-	  decode_json $jsonResponse; #decode json using module and gets a hash reference number
+	my $hashRef = decode_json $jsonResponse or die print $url; #decode json using module and gets a hash reference number
 	my %hash = %$hashRef;        #deference hash number to a hash
-
 	if ( $response->is_success ) {
 		my @arrayResult = @{ $hash{"results"} };
 		my $street;
 		my $breakFlag = 0;
 		if (@arrayResult) {
 			foreach (@arrayResult) {
-				foreach ($%{$_}{"address_components"}) {
-					if($%{$_}->{"types"}[0] eq "route") {
-						$street = $%{$_}->{"short_name"};
+				my $resultHashRef = $_;
+				#print $resultHashRef;
+				foreach (@{$resultHashRef->{"address_components"}}) {
+					my $nestedHashRef = $_;
+					#print $nestedHashRef;
+					if($nestedHashRef->{"types"}[0] eq "route") {
+						$street = $nestedHashRef->{"short_name"};
 						$breakFlag= 1;
 						last;
 					}
@@ -73,9 +71,9 @@ while ( my $rowHash = $sth->fetchrow_hashref() ) {
 					last;
 				}
 			}
-			#if ($breakFlag == 1) {
+			if ($breakFlag == 1) {
 				$sth2->execute($street,$index) or die "Couldn't execute statement: " . $sth2->errstr;
-			#}
+			}
 		}
 	}
 }
