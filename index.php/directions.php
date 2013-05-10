0b0aa1d8c28d73edf3cfd379af36a1660c183cc4
Kevin Hsieh <hsieh63@eden.rutgers.edu>
2013-05-10 14:17:06 -0400

    Validation for directions input boxes

--- index.php/directions.php
+++ index.php/directions.php
@@ -1,23 +1,37 @@
 <?php include 'header.php'; ?>
 <script>
 $(document).ready(function() {
-    $('#directionsImageData').submit(function() {
-        //alert($('#mapImageData').serialize());
-        $.ajax({
-            url: "directionsGeocoding.php",
-            type: "POST",
-            data: $('#directionsImageData').serialize(),
-            success: function(response) {
-                //$('#intensityColorTble').show();
-                $('#directionsImageData').find('#formResult').html(response);
-                //alert('Success');
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                //alert('Error');
-            }
-        });
-    return false;
+    $('#directionsImageData').validate({
+        rules: {
+            sLocation : "required",
+            destination : "required",
+            time : "required"
+        },
+        messages: {
+            sLocation : "Please enter a start location.",
+            destination : "Please enter a destination.",
+            time : "Please select a time"
+        },
+        submitHandler: function(form) {
+            $.ajax({
+                url: "directionsGeocoding.php",
+                type: "POST",
+                data: $('#directionsImageData').serialize(),
+                success: function(response) {
+                    //$('#intensityColorTble').show();
+                    $('#directionsImageData').find('#formResult').html(response);
+                    //alert('Success');
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    //alert('Error');
+                }
+            });
+        },
+        invalidHandler: function(event, validator) {
+            return false;
+        }
     });
+    
     var startBox = (document.getElementById('startLoc'));
     var options = {
         types: ['geocode'],
@@ -46,20 +60,45 @@ $(document).ready(function() {
 	Testing
 	<br>
 	<form id="directionsImageData" action="" method="">
-	Starting Location: <input type="text" name="sLocation" id="startLoc" size = "50">
-	Destination: <input type="text" name="destination" id="endLoc" size = "50">
-	<select name="time" required>
-            <option value="-1" selected="selected" disabled>Estimated Leaving Time:</option>
-            <option value="0">12am-3am</option>
-            <option value="1">3am-6am</option>
-            <option value="2">6am-9am</option>
-            <option value="3">9am-12pm</option>
-            <option value="4">12am-3pm</option>
-            <option value="5">3pm-6pm</option>
-            <option value="6">6pm-9pm</option>
-            <option value="7">9pm-12am</option>
-    </select>
-	<input type="submit" value="Submit">
+        <table>
+            <tr>
+                <td>
+	                Starting Location: <input type="text" name="sLocation" id="startLoc" size = "50">
+                </td>
+                <td>
+	                Destination: <input type="text" name="destination" id="endLoc" size = "50">
+                </td>
+                <td>
+	                <select name="time">
+                            <option value="-1" selected="selected" disabled>Estimated Leaving Time:</option>
+                            <option value="0">12am-3am</option>
+                            <option value="1">3am-6am</option>
+                            <option value="2">6am-9am</option>
+                            <option value="3">9am-12pm</option>
+                            <option value="4">12am-3pm</option>
+                            <option value="5">3pm-6pm</option>
+                            <option value="6">6pm-9pm</option>
+                            <option value="7">9pm-12am</option>
+                    </select>
+                </td>
+                <td>
+	                <input type="submit" value="Submit">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label class="error" for="startLoc" style="color:#FF0000;"></label>
+                </td>
+                <td>
+                    <label class="error" for="endLoc" style="color:#FF0000;"></label>
+                </td>
+                <td>
+                    <label class="error" for="time" style="color:#FF0000;"></label>
+                </td>
+                <td>
+                </td>
+            </tr>
+        </table>
 	<div id="formResult"> </div>
     </form>
 
